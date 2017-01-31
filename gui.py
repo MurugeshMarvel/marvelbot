e46b5443ec2ad639fe1e33fa371c663efea776a9
MurugesMarvel <murugeshmarvel95@zoho.com>
2017-01-31 12:04:49 +0530

    added gui section for the bot interference

--- gui.py
+++ gui.py
@@ -1,10 +1,42 @@
 import wx
 
-def ip():
-    app = wx.PySimpleApp()
-    dialog = wx.TextEntryDialog(None, 
-            "Enter you message here.",
-            "Text Entry", "", style=wx.OK|wx.CANCEL)
-    if dialog.ShowModal() == wx.ID_OK:
-        return dialog.GetValue()
+
+class MyApp(wx.App):
+	def OnInit(self):
+		frame = MyFrame("Hello World",(50,60),(450,340))
+		frame.Show()
+		self.SetTopWindow(frame)
+		return True
+
+class MyFrame(wx.Frame):
+	def __init__(self,title,pos,size):
+		wx.Frame.__init__(self,None,-1, title,pos,size)
+		menuFile = wx.Menu()
+		menuFile.Append(1, "&About")
+		menuFile.AppendSeparator()
+		menuFile.Append(2, "&Exit")
+		menuBar = wx.MenuBar()
+		menuBar.Append(menuFile,"&File")
+		self.SetMenuBar(menuBar)
+		self.CreateStatusBar()
+		self.SetStatusText("Welcome to wxPython")
+		self.Bind(wx.EVT_MENU, self.OnAbout, id=1)
+		self.Bind(wx.EVT_MENU, self.OnQuit, id=2)
+
+
+	def OnQuit(self,event):
+		self.Close()
+
+	def OnAbout(self,event):
+		wx.MessageBox("This is a simple Wxpython hello World sample","About Hello world",wx.OK|wx.ICON_INFORMATION,self)
+
+
+	def inputbox():
+    	app = wx.PySimpleApp()
+    	dialog = wx.TextEntryDialog(None, 
+            	"Enter you message here.",
+            	"Text Entry", "", style=wx.OK|wx.CANCEL)
+    	if dialog.ShowModal() == wx.ID_OK:
+        	return dialog.GetValue()
     
+        	
